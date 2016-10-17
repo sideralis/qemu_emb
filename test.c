4831a582d6f28562bc53d55388e215db81c57d18
Bernard Gautier <bernard.gautier@intel.com>
2016-10-17 15:09:15 +0200

    print without Makefile
    Compile, link and run with: 
    qemu-system-arm -M realview-eb -m 128M -nographic -kernel test.bin

---
+++ test.c
@@ -0,0 +1,14 @@
+volatile unsigned int * const UART0DR = (unsigned int *) 0x10009000;
+
+void print_uart(const char *s)
+{
+	while (*s != '\0') { /* Loop until end of string */
+		*UART0DR = (unsigned int) (*s); /* Transmit char */
+		s++; /* Next char */
+	}
+}
+
+void c_entry()
+{
+	print_uart("Hello world!\n");
+}
